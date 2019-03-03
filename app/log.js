ad9d82b99a8f8c76e5ed6e6e16516e4a53e246cc
Conrado Gouvea <conradoplg@gmail.com>
2019-03-03 14:59:21 -0300

    limite log size

--- app/log.js
+++ app/log.js
@@ -1,6 +1,10 @@
 var log = require('winston')
 
 log.level = 'debug'
-log.add(log.transports.File, {filename: 'quiriquiri.log'});
+log.add(new log.transports.File({
+    filename: 'quiriquiri.log',
+    maxsize: 1024 * 1024,
+    maxFiles: 5
+}))
 
 module.exports = log
